7ad7c5cb27935a9031e7ded87c1eaa0f56cbb75d
Kim Alvefur <zash@zash.se>
2013-03-31 13:30:35 +0200

    compliance.lua: add xep0073 to for clients

--- compliance.lua
+++ compliance.lua
@@ -7,6 +7,7 @@ xep0270 = xep0270 and xep0016 and xep0191 and xep0124 and xep0206 and xep0054 an
 xep0302 = rfc6120 and rfc6121 and rfc6122 and xep0030 and xep0114 and "Core Server" or false
 xep0302 = xep0302 and xep0045 and xep0054 and xep0115 and xep0124 and xep0163 and xep0191 and xep0198 and xep0206 and "Advanced Server" or xep0302 or false
 elseif type == "client" then
+xep0073 = rfc3920 and rfc3921 and xep0030 and xep0077 and xep0078 and xep0086 and xep0115 and true
 xep0302 = rfc6120 and rfc6121 and rfc6122 and xep0030 and xep0115 and "Core Client" or false
 xep0302 = xep0302 and xep0045 and xep0054 and xep0085 and xep0184 and xep0163 and xep0198 and "Advanced Client" or xep0302 or false
 end
