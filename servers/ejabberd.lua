056f9c9d5c33ad1feab3f7a0ded8e05a11bbc087
Michael Vetter <jubalh@openmailbox.org>
2016-03-08 13:36:06 +0100

    Update ejabberd capabilities
    Updated according to: https://www.process-one.net/en/ejabberd/protocols/

--- servers/ejabberd.lua
+++ servers/ejabberd.lua
@@ -4,17 +4,17 @@ ipv6 = "prefers IPv4 by default"
 admin_web = true
 rfc3920 = true
 rfc3921 = true
-rfc6120 = "Maybe?"
-rfc6121 = "Maybe?"
+rfc6120 = true
+rfc6121 = true
+rfc6122 = true
 xep0004 = true
 xep0012 = true
-xep0013 = ">=16.02"
+xep0013 = ">=16.02 only on eBE"
 xep0016 = true
 xep0018 = ">=2.1.0, <3.0.0"
-xep0020 = false
-xep0022 = true
+xep0020 = true
 xep0023 = true
-xep0025 = true
+xep0027 = true
 xep0030 = true
 xep0033 = ">=3.0.0"
 xep0039 = true
@@ -27,7 +27,7 @@ xep0059 = ">=2.1.0"
 xep0060 = true
 xep0065 = true
 xep0070 = false
-xep0077 = "partially"
+xep0077 = true
 xep0078 = true
 xep0079 = "contrib"
 xep0082 = ">=2.1.0"
@@ -36,6 +36,7 @@ xep0086 = true
 xep0090 = true
 xep0091 = true
 xep0092 = true
+xep0100 = "only on eBE"
 xep0106 = true
 xep0114 = true
 xep0115 = ">=2.1.4"
@@ -54,30 +55,40 @@ xep0158 = ">=2.1.0"
 xep0160 = true
 xep0163 = true
 xep0170 = true
-xep0172 = false
+xep0172 = "only on eBE"
 xep0175 = true
 xep0176 = ">=2.1.0"
-xep0178 = false
+xep0178 = "S2S Auth only for eCS, true vor eBE"
+xep0190 = ">=2.1.7"
 xep0191 = ">=2.1.7"
 xep0193 = "contrib"
 xep0198 = true
 xep0199 = ">=2.1.0"
 xep0202 = ">=2.1.0"
 xep0203 = ">=2.1.0"
-xep0205 = "partially"
+xep0205 = "only on eBE"
 xep0206 = true
 xep0212 = true
-xep0215 = false
+xep0215 = "only on eBE"
 xep0216 = true
-xep0223 = "partially"
-xep0225 = false
+xep0220 = "only on eBE"
+xep0222 = true
+xep0223 = true
+xep0225 = "only on eBE"
 xep0227 = ">=2.1.0"
 xep0237 = ">=2.1.0"
 xep0243 = true
 xep0248 = ">=2.1.0"
+xep0256 = "only on eBE"
 xep0270 = ">=2.1.7"
-xep0278 = "contrib"
+xep0278 = true
 xep0279 = ">=2.1.3"
 xep0279 = true
 xep0280 = ">2.1.12"
-xep0313 = "contrib, >=14.12"
+xep0297 = true
+xep0302 = true
+xep0313 = true
+xep0334 = true
+xep0352 = true
+xep0357 = "contrib"
+xep0363 = "contrib"
